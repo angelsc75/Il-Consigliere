c34e6df3400dde3c1e3f85af154df718ed8f1ca7
angelsc75 <asc7es@gmail.com>
2025-01-04 16:05:04 +0100

    arreglo header

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -2,13 +2,24 @@
 import React from "react";
 import "./header.css"; // Archivo CSS para los estilos
 
-const Header = () => {
+const Header = ({ userId, onLogout }) => {
   return (
     <header className="app-header">
-      <img src="../logo.png" alt="Il Consigliere" className="app-logo" />
-      <h1 >Il Consigliere</h1>
-    </header>
+  <img src="../logo.png" alt="Il Consigliere" className="app-logo" />
+  <h1>Il Consigliere</h1>
+  <div className="header-actions">
+    {userId && (
+      <div className="user-info">
+        <span className="user-id">User ID: {userId}</span>
+        <button className="logout-button" onClick={onLogout}>
+          Salir
+        </button>
+      </div>
+    )}
+  </div>
+</header>
   );
 };
 
 export default Header;
+
